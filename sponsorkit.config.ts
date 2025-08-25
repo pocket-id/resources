f87047f6dd12de8198689bb9ff38e94a336c2b35
Kyle Mendell <ksm@ofkm.us>
2025-08-25 14:49:03 -0500

    chore: first commit, wip not ready yet

---
+++ sponsorkit.config.ts
@@ -0,0 +1,143 @@
+import { BadgePreset, defineConfig, presets } from 'sponsorkit';
+import fs from 'fs/promises';
+
+const past: BadgePreset = {
+  avatar: {
+    size: 20,
+  },
+  boxWidth: 22,
+  boxHeight: 22,
+  container: {
+    sidePadding: 35,
+  },
+};
+
+export default defineConfig({
+  tiers: [
+    {
+      title: 'Past Sponsors',
+      monthlyDollars: -1,
+      preset: past,
+    },
+    {
+      title: 'Backers',
+      preset: presets.small,
+    },
+    {
+      title: 'Sponsors',
+      monthlyDollars: 10,
+      preset: {
+        avatar: {
+          size: 42,
+        },
+        boxWidth: 52,
+        boxHeight: 52,
+        container: {
+          sidePadding: 30,
+        },
+      },
+    },
+    {
+      title: 'Silver Sponsors',
+      monthlyDollars: 50,
+      preset: presets.medium,
+    },
+    {
+      title: 'Gold Sponsors',
+      monthlyDollars: 100,
+      preset: presets.large,
+    },
+    {
+      title: 'Platinum Sponsors',
+      monthlyDollars: 500,
+      preset: presets.xl,
+    },
+  ],
+
+  sponsorsAutoMerge: true,
+
+  mergeSponsors: [
+    [{ login: 'patak-dev', provider: 'github' }],
+    [{ login: 'serkodev', provider: 'github' }],
+    [{ login: 'WebWorkerTech', provider: 'github' }],
+    [{ login: 'kissu', provider: 'github' }],
+  ],
+
+  async onSponsorsReady(sponsors) {
+    await fs.writeFile(
+      'sponsors.json',
+      JSON.stringify(
+        sponsors
+          .filter((i) => i.privacyLevel !== 'PRIVATE')
+          .map((i) => {
+            return {
+              name: i.sponsor.name,
+              login: i.sponsor.login,
+              avatar: i.sponsor.avatarUrl,
+              amount: i.monthlyDollars,
+              link: i.sponsor.linkUrl || i.sponsor.websiteUrl,
+              org: i.sponsor.type === 'Organization',
+            };
+          })
+          .sort((a, b) => b.amount - a.amount),
+        null,
+        2
+      )
+    );
+  },
+
+  onSponsorsAllFetched(sponsors) {
+    sponsors.unshift({
+      monthlyDollars: 2000,
+      privacyLevel: 'PUBLIC',
+      sponsor: {
+        name: 'NuxtLabs',
+        login: 'nuxtlabs',
+        linkUrl: 'https://nuxtlabs.com',
+        avatarUrl: 'https://avatars.githubusercontent.com/u/62017400?v=4',
+        type: 'Organization',
+      },
+    });
+    return sponsors;
+  },
+
+  outputDir: '.',
+  formats: ['svg', 'png'],
+
+  renders: [
+    {
+      name: 'sponsors',
+      width: 800,
+      filter: (sponsor) => sponsor.sponsor?.login !== 'nuxtlabs',
+    },
+    {
+      name: 'sponsors.wide',
+      width: 1800,
+      filter: (sponsor) => sponsor.sponsor?.login !== 'nuxtlabs',
+    },
+    {
+      name: 'sponsors.part1',
+      width: 800,
+      filter: (sponsor) => sponsor.monthlyDollars >= 9.9 && sponsor.sponsor?.login !== 'nuxtlabs',
+    },
+    {
+      name: 'sponsors.part2',
+      width: 800,
+      filter: (sponsor) => sponsor.monthlyDollars < 9.9 && sponsor.monthlyDollars >= 0,
+    },
+    {
+      name: 'sponsors.past',
+      width: 800,
+      filter: (sponsor) => sponsor.monthlyDollars < 0,
+    },
+    {
+      name: 'sponsors.circles',
+      width: 1000,
+      includePastSponsors: true,
+      renderer: 'circles',
+      circles: {
+        radiusPast: 3,
+      },
+    },
+  ],
+});
